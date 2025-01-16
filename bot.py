4f011b5608ef37a09f42eb282ea7b7f743ebd602
crgonzalezf <31080616+crgonzalezf@users.noreply.github.com>
2025-01-16 13:06:30 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,15 +23,13 @@ def build_prompt(user: User, context: str):
 
 @traceable(run_type="tool", name="get_movie_trailer")
 def get_movie_trailer_response(client, movie_title, user):
-    """
-    Genera una respuesta con el enlace al tráiler de una película.
-    """
+    
     trailer_link = get_movie_trailer(movie_title)
 
     if not trailer_link:
         return f"Lo siento, no pude encontrar un tráiler para {movie_title}. Intenta buscarlo manualmente en YouTube."
 
-    return f"Puedo ayudarte con esto. Aquí tienes el tráiler de '{movie_title}': {trailer_link}"
+    return f"No puedo mostrarte directamente el trailer, pero si puedo entregarte el enlace de '{movie_title}': {trailer_link}"
 
 @traceable(run_type="tool", name="where_to_watch")
 def where_to_watch(client: OpenAI, search_term: str, user: User):
