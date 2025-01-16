aa2ee9f4e5266a434200775f1c730b256fbd0718
crgonzalezf <31080616+crgonzalezf@users.noreply.github.com>
2025-01-16 11:59:42 -0300

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,39 +1,3 @@
-//from simplejustwatchapi.justwatch import search as justwatch_search
-import tmdbsimple as tmdb
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
-tmdb.API_KEY = getenv('TMDB_API_KEY')
-tmdb.ACCESS_TOKEN = getenv('TMDB_ACCESS_TOKEN')
-
-def search(movie_name):
-    search = tmdb.Search()
-    response = search.multi(query=movie_name, language='es-CL')
-
-    if not search.results:
-        return None
-
-    return search.results[0]
-
-
-def search_platforms(movie_name):
-    results = justwatch_search(movie_name, "CL", "es")
-    platforms = []
-
-    if not results:
-        return platforms
-
-    for offer in results[0].offers:
-        platforms.append({
-            'name': offer.package.name,
-            'icon': offer.package.icon,
-            'url': offer.url,
-        })
-
-    return platforms//
-
-
 from simplejustwatchapi.justwatch import search as justwatch_search
 import tmdbsimple as tmdb
 from dotenv import load_dotenv
